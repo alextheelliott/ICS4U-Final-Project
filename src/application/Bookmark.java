49a28ebf5eae0a7720b5ceb51f55bcb8e6e22796
JX0Z <jaxhodgkinson@crescentschool.org>
2020-05-22 13:53:25 -0400

    Removed unused imports

--- src/application/Bookmark.java
+++ src/application/Bookmark.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import static application.Constants.DataConstants.*;
--- src/buttons/HelpButton.java
+++ src/buttons/HelpButton.java
@@ -1,7 +1,5 @@
 package buttons;
 
-import javax.swing.plaf.FontUIResource;
-
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
