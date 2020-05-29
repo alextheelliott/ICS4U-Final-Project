2ae3e136543c2e5871ec8288039488e1abdb3ba0
JX0Z <jaxhodgkinson@crescentschool.org>
2020-05-29 19:35:05 -0400

    Commented Bookmark class

--- src/application/Bookmark.java
+++ src/application/Bookmark.java
@@ -11,28 +11,32 @@ import java.util.HashMap;
 import static application.Constants.DataConstants.*;
 
 public class Bookmark implements Comparable<Bookmark> {
+    // Each bookmark has a url (website), topic (folder), and a title associated with it. These are used for display purposes
     private String url;
     private String topic;
     private String title;
 
+    // getBookmarks returns a sorted nested ArrayList of Bookmarks for use outside this class
     public static ArrayList<ArrayList<Bookmark>> getBookmarks() {
         ArrayList<Bookmark> list = loadCSV();
         ArrayList<ArrayList<Bookmark>> finalList = new ArrayList<ArrayList<Bookmark>>();
         insSort(list);
         HashMap<String, ArrayList<Bookmark>> topics = new HashMap<String, ArrayList<Bookmark>>();
 
+        // Places each Bookmark from the csv file into a hashmap of ArrayLists. The key is the topic, while the value is an ArrayList of Bookmarks
         for (Bookmark i : list) {
             if (!topics.containsKey(i.getTopic()))
                 topics.put(i.getTopic(), new ArrayList<Bookmark>());
             topics.get(i.getTopic()).add(i);
         }
-
+        // Moves the hashmap into an ArrayList for easy use
         for (ArrayList<Bookmark> i : topics.values()) {
             finalList.add(i);
         }
         return finalList;
     }
 
+    // removeBookmark removes a Bookmark from the csv file. 
     public static void removeBookmark(Bookmark toRemove) {
         ArrayList<Bookmark> bookmarks = loadCSV();
         for (int i = 0; i < bookmarks.size(); i++) {
@@ -43,18 +47,21 @@ public class Bookmark implements Comparable<Bookmark> {
         saveCSV(bookmarks);
     }
 
+    // loadCSV returns an ArrayList of Bookmarks to be used in GetBookmarks & removeBookmark
     public static ArrayList<Bookmark> loadCSV() {
+        // Reads data from the file
         try {
             BufferedReader br = new BufferedReader(new FileReader(DATA_FILE));
             ArrayList<Bookmark> list = new ArrayList<Bookmark>();
             String line = br.readLine();
             while (line != null) {
+                // Enters the data based on 3 scenarios
                 String[] data = line.split(",");
-                if (data.length == 3) {
+                if (data.length == 3) { // No missing data so no special requirements
                     list.add(new Bookmark(data[0], data[1], data[2]));
-                } else if (data.length == 2) {
+                } else if (data.length == 2) { // Topic is missing, so it is entered as blank
                     list.add(new Bookmark(data[0], "", data[1]));
-                } else if (data.length == 1) {
+                } else if (data.length == 1) { // Only the URL is available, so topic is blank and title is set to URL
                     list.add(new Bookmark(data[0], "", data[0]));
                     System.out.println(Arrays.toString(data));
                 }
@@ -67,10 +74,12 @@ public class Bookmark implements Comparable<Bookmark> {
         }
     }
 
+    // saveCSV saves the ArrayList of bookmarks back to the datafile
     public static void saveCSV(ArrayList<Bookmark> list) {
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(DATA_FILE));
             for (int i = 0; i < list.size(); i++) {
+                // Writes the file in the format "URL","Topic","Title"
                 bw.write(list.get(i).getUrl() + "," + list.get(i).getTopic() + "," + list.get(i).getTitle() + "\n");
             }
             bw.flush();
@@ -80,10 +89,12 @@ public class Bookmark implements Comparable<Bookmark> {
         }
     }
 
-    public static void insSort(ArrayList<Bookmark> arr) {
+    // insSort sorts the Bookmarks using insertion sort on the title for use in getBookmarks()
+    private static void insSort(ArrayList<Bookmark> arr) {
         for (int i = 1; i < arr.size(); i++) {
             int j = i;
             while (j > 0 && arr.get(j).compareTo(arr.get(j - 1)) < 0) {
+                // Swaps Bookmarks at position j & j-1
                 Bookmark temp = arr.get(j);
                 arr.set(j, arr.get(j - 1));
                 arr.set(j - 1, temp);
@@ -120,17 +131,17 @@ public class Bookmark implements Comparable<Bookmark> {
 
     // Overridden methods
     @Override
-    public String toString() {
+    public String toString() { // Useful for debugging
         return ("[URL: \"" + this.getUrl() + "\" Title: \"" + this.getTitle() + "\" Topic: \"" + this.getTopic())
                 + "\"]";
     }
 
     @Override
-    public int compareTo(Bookmark bm) {
+    public int compareTo(Bookmark bm) { // Allows bookmarks to be sorted by title
         return this.getTitle().compareTo(bm.getTitle());
     }
 
-    public boolean equals(Bookmark bm) {
+    public boolean equals(Bookmark bm) { // Makes it easy to check if 2 bookmarks are the same
         if (!this.getTitle().equals(bm.getTitle())) {
             return false;
         } else if (!this.getTopic().equals(bm.getTopic())) {
@@ -141,7 +152,7 @@ public class Bookmark implements Comparable<Bookmark> {
         return true;
     }
 
-    // Constructors
+    // Constructor
     public Bookmark(String url, String topic, String title) {
         this.url = url;
         this.topic = topic;
